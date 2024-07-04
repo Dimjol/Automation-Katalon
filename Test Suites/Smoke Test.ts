fc3ade8899c2fe0cdb06e29d84a19b67d675c3b8
DIMAS MAHESA <96423520+Dimjol@users.noreply.github.com>
2024-07-04 22:15:46 +0700

    Test Smoke

---
+++ Test Suites/Smoke Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3199241a-04ca-4b83-86a5-6b0727135b89</testSuiteGuid>
+</TestSuiteEntity>
